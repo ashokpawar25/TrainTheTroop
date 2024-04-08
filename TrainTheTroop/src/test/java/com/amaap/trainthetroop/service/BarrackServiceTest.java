250bc6a5c1a26504b94038b8efb9c45d56744084
ashokpawar25 <ashokpawar25052001@gmail.com>
2024-04-09 01:29:39 +0530

    Added test cases to get the specific count of archer and barbarian

--- TrainTheTroop/src/test/java/com/amaap/trainthetroop/service/BarrackServiceTest.java
+++ TrainTheTroop/src/test/java/com/amaap/trainthetroop/service/BarrackServiceTest.java
@@ -2,10 +2,13 @@ package com.amaap.trainthetroop.service;
 
 import com.amaap.trainthetroop.domain.model.Trooper;
 import com.amaap.trainthetroop.domain.model.Weapon;
+import com.amaap.trainthetroop.domain.model.exception.InvalidTrooperDataException;
+import com.amaap.trainthetroop.domain.model.factory.TrooperFactory;
 import com.amaap.trainthetroop.repository.Impl.InMemoryArmyCampRepository;
 import com.amaap.trainthetroop.repository.Impl.InMemoryBarrackRepository;
 import com.amaap.trainthetroop.repository.Impl.InMemoryTrooperRepository;
 import com.amaap.trainthetroop.repository.Impl.db.impl.FakeInMemoryDatabase;
+import com.amaap.trainthetroop.service.exception.BarrackFullException;
 import com.amaap.trainthetroop.service.exception.InvalidTrooperTypeException;
 import com.amaap.trainthetroop.service.model.TroopType;
 import org.junit.jupiter.api.Test;
